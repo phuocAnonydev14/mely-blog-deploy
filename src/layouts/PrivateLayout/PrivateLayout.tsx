1fe83539d8c92369acb5f51f2f5dc1cd013f84cd
anonyDev <phuochh@nortstudio.vn>
2024-11-01 23:28:39 +0700

    chore: fix build

---
+++ src/layouts/PrivateLayout/PrivateLayout.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { PropsWithChildren, useEffect } from 'react';
+import { LOGIN } from '@/common/constants/path.constant';
+import { ENotification } from '@/common/utils/notification.util';
+import { notification } from 'antd';
+import useUser from '@/hooks/useUser';
+import {Header} from "@/components/layout/Header/Header";
+
+export default function PrivateLayout({ children }: PropsWithChildren) {
+  const { isLoggedIn, isLoading } = useUser();
+  const [api, contextHolder] = notification.useNotification();
+
+  useEffect(() => {
+    if (!isLoading && !isLoggedIn) {
+      api.open({ type: ENotification.WARNING, message: 'You must login to use this service' });
+      window.location.href = LOGIN;
+      return;
+    }
+  }, [api, isLoading, isLoggedIn]);
+
+  return (
+    <div>
+      {contextHolder}
+      <Header />
+      {children}
+    </div>
+  );
+}
