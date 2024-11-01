26d23c3c980b0a362691c74dbe9e8d2211c5673d
anonyDev <phuochh@nortstudio.vn>
2024-11-01 23:38:54 +0700

    deploy

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from 'react';
-import Header from '@/components/layout/Header';
+import {Header} from "@/components/layout/Header/Header";
 
 export default function MainLayout({ children }: PropsWithChildren) {
   return (
