2c2d40cc8d463f1a35fe054e4ec2d95b1e754baf
anonyDev <phuochh@nortstudio.vn>
2024-11-01 23:34:37 +0700

    deploy

--- src/components/layout/Header/index.ts
+++ src/components/layout/Header/index.ts
@@ -1,4 +1,3 @@
-export { default } from '@/components/layout/Header/Header';
 export { default as UserAvatar } from '@/components/layout/Header/UserAvatar/UserAvatar';
 export { default as SearchInput } from '@/components/layout/Header/SearchInput/SearchInput';
 export { default as Notification } from '@/components/layout/Header/Notification/Notification';
